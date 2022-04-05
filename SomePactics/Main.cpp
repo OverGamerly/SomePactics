6094faaf74f47b26bec84146b2095c6c8bddb872
Кирилл Дрозд <kirilldrozd15@gmail.com>
2022-04-05 16:11:38 +0300

    Debug our programm with first task

--- SomePactics/Main.cpp
+++ SomePactics/Main.cpp
@@ -1,23 +1,23 @@
-#include "MainHead.h"
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-	int* number = new int;
+	int number;
 	int count = 5;
 
 	cout << "Enter your number: ";
-	cin >> *number;
+	cin >> number;
 
-	while (*number != 0)
+	while (number != 0)
 	{
-		cout << count << " numb: " << *number % 10 << endl;
+		cout << count << " numb: " << number % 10 << endl;
 		
 		count--;
 
-		*number /= 10;
+		number /= 10;
 	}
-
-	delete number;
 
 	return 0;
 }
